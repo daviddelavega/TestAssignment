9d06e634955d9ef33f26dd82004f1b977651a20f
daviddelavega <dave.delavega@gmail.com>
2023-05-21 07:43:34 -0500

    alertCriteria Mutator

--- ThermometerLogic/TemperatureAlertLogic.cs
+++ ThermometerLogic/TemperatureAlertLogic.cs
@@ -10,55 +10,7 @@ namespace TemperatureAlertSystem.ThermometerLogic
     * not to alert a caller.
     */
     public class TemperatureAlertLogic
-    {     
-        public static bool TemperatureUpdate(int consumerId, float currentTemperature, float previousTemperature, Criterion criterion)
-        {            
-            bool alertConsumer = false;   
-            float arbitraryThreshold = criterion.ArbitraryThreshold;
-            float insignificantFluctuation = criterion.InsignificantFluctuation;
-            Direction direction = criterion.Direction;          
-
-            if (direction != Direction.None)
-            {
-                if (direction == Direction.Rising && previousTemperature < arbitraryThreshold && currentTemperature >= arbitraryThreshold)
-                {
-                    alertConsumer = true;                    
-                }
-                else if (direction == Direction.Falling && previousTemperature > arbitraryThreshold && currentTemperature <= arbitraryThreshold)
-                {
-                    alertConsumer = true;                  
-                }                
-            }
-            else if (Math.Abs(currentTemperature - arbitraryThreshold) > insignificantFluctuation)
-            {
-                alertConsumer = true;               
-            }      
-
-            if (alertConsumer) 
-            {
-                Console.WriteLine($"\n***************\n" +
-                        $"Consumer{consumerId}'s Threshold Breached!!: " +
-                        $"\nArbitraryThreshold:{arbitraryThreshold}°C" +
-                        $"\nInsignificantFluctuation:+/-{insignificantFluctuation}°C" +
-                        $"\nDirection:{direction}" +
-                        $"\nPreviousTemperature:{previousTemperature}°C" +
-                        $"\nCurrentTemperature:{currentTemperature}°C");               
-            }
-            else
-            {
-                Console.WriteLine($"\n***************\n" +
-                        $"Consumer{consumerId}'s Threshold NOT! breached: " +
-                        $"\nArbitraryThreshold:{arbitraryThreshold}°C" +
-                        $"\nInsignificantFluctuation:+/-{insignificantFluctuation}°C" +
-                        $"\nDirection:{direction}" +
-                        $"\nPreviousTemperature:{previousTemperature}°C" +
-                        $"\nCurrentTemperature:{currentTemperature}°C");
-            }
-
-            return alertConsumer;
-            
-        }
-
+    {
         public static List<Temperature> CheckTemperature(int consumerId, List<float> temperatures, Criterion criterion)
         {         
             var temperatureList = new List<Temperature>();
